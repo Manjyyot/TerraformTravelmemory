628fa147e7e6419bc6bc35c623685567c4746d68
Manjyyot <manjyotsinghchaudhary@gmail.com>
2025-03-09 17:05:41 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,15 @@ output "frontend_instance_id" {
 output "backend_instance_id" {
   value = aws_instance.backend_server.id
 }
+
+output "mongodb_instance_ip" {
+  value = aws_instance.mongodb_server.public_ip
+}
+
+output "frontend_instance_ip" {
+  value = aws_instance.frontend_server.public_ip
+}
+
+output "backend_instance_ip" {
+  value = aws_instance.backend_server.public_ip
+}
